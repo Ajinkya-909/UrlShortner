a76f019eb8a8ccd95be029777a228c10c7f5e4c5
Ajinkya Deshmukh <ajinkyadeshmukh8686@gmail.com>
2026-03-12 09:44:02 +0530

    Creating schema

--- models/user.model.js
+++ models/user.model.js
@@ -1,8 +1,13 @@
-import { sql } from "drizzle-orm";
 import { uuid } from "drizzle-orm/gel-core";
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer,text, pgTable, varchar, timestamp } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
-  id: uuid().primaryKey()
-  
+  id: uuid().primaryKey(),
+  firstname: varchar('first_name', {length:20}).notNull(),
+  lastname: varchar('last_name', {length:20}),
+  email: varchar( {length:200}).notNull().unique(),
+  password: text().notNull(),
+  salt: text().notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').$onUpdate(() => new Date()),
 });

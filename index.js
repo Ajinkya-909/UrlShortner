bbf7ae78d6adfddc3efa44ff21777789284cbde1
Ajinkya Deshmukh <ajinkyadeshmukh8686@gmail.com>
2026-03-13 07:10:44 +0530

    Auth Middleware set

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import userRouter from './routes/user.routes.js'
+import {authenticationMiddleware} from './middlewares/auth.middleware.js'
 
 const app = express();
 
 app.use(express.json())
+app.use(authenticationMiddleware)
 
 app.use('/user',userRouter)
 
